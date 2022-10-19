edd3727bf99f4102da8b7328070af2f8c3e6a170
IlinaGergova <ili.gergova@gmail.com>
2022-10-19 15:49:32 +0300

    getHeroes add

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -30,4 +30,9 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroesArr = heroes);
+  }
 }
